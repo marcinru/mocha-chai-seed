7c8be644a701fdcee521585de7e73b3251948a66
Marcin Rudzki <mar.rudzki@gmail.com>
2017-02-23 18:50:50 +0100

    Make PrimeChecker pass tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 export class PrimeChecker {
-    constructor() {
-      
-    };
+    constructor() {};
+
+    check(val) {
+        if (isNaN(val) || val === undefined) {
+            throw new Error('Invalid argument')
+        } else {
+            if (val === 1) {
+                return false;
+            } else {
+                for (let i = 2; i < Math.sqrt(val); i++) {
+                    if (val % i === 0) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
 }
--- test/tests.spec.js
+++ test/tests.spec.js
@@ -1,38 +1,33 @@
 import { expect } from 'chai';
 import { PrimeChecker } from '../src/index.js';
 
-describe("PrimeChecker class under test", () => {
-
+describe('PrimeChecker', () => {
   let checker;
 
   beforeEach(() => {
     checker = new PrimeChecker();
   });
 
-  it("should pass this test test", () => {
-    expect(true).to.eql(true)
-  })
-
-  it("should return false when passed 1", () => {
+  it('should return false when passed 1', () => {
     expect(checker.check(1)).to.eql(false);
   })
 
-  it("should return true when passed 2", () => {
+  it('should return true when passed 2', () => {
     expect(checker.check(2)).to.eql(true);
   })
 
-  it("should return false when passed 8", () => {
+  it('should return false when passed 8', () => {
     expect(checker.check(8)).to.eql(false);
   })
 
-  it("should throw an error when called without a value", () => {
-    var call = () => {checker.check();}
-    expect(call).to.throw(Error,'Invalid argument');
+  it('should throw an error when called without a value', () => {
+    let call = () => { checker.check() }
+    expect(call).to.throw(Error, 'Invalid argument');
   })
 
-  it("should throw an error when passed a value that is not a number", () => {
-    var call = () => {checker.check("piespies");}
-    expect(call).to.throw(Error,'Invalid argument');
+  it('should throw an error when passed a value that is not a number', () => {
+    let call = () => { checker.check('lorem ipsum') }
+    expect(call).to.throw(Error, 'Invalid argument');
   })
 
 })
